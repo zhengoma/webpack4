ae489f435745648c3ffaa5582964b233a5eac795
马远征 <huatian.myz@raycloud.com>
2021-01-28 14:39:40 +0800

    UPDATE:source-map

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,6 +3,7 @@ const common = require('./webpack.common.js');
 const CompressionWebpackPlugin = require('compression-webpack-plugin');
 module.exports = merge(common, { // 将webpack.common.js合并到当前文件
     mode: 'production', // 模式配置
+    devtool: 'source-map',
     plugins: [
         new CompressionWebpackPlugin({
             filename: '[path].gz[query]',
